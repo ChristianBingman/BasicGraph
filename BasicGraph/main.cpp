6898f9bd5f381fa34af5b76f2d0283427a345ff1
ChristianBingman <christianbingman@gmail.com>
2020-04-16 13:10:54 -0500

    Initial Commit

---
+++ BasicGraph/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BasicGraph
+//
+//  Created by Christian Bingman on 4/16/20.
+//  Copyright © 2020 Christian Bingman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
